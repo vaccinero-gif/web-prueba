b7f69cda5d20830dfaf15f0f17ff3abd0fb6995f
vaccinero-gif <vaccinero@gmail.com>
2026-03-28 19:46:52 +0100

    Update routing.ts

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,9 +1,7 @@
-import {defineRouting} from 'next-intl/routing';
-
-export const routing = defineRouting({
-  locales: ['en', 'es', 'fr', 'de', 'it', 'pt'],
-  defaultLocale: 'en'
-});
+export const routing = {
+  locales: ['en', 'es', 'fr', 'de', 'it', 'pt'] as const,
+  defaultLocale: 'en' as const
+};
 
 export type AppLocale = (typeof routing.locales)[number];
 export const activeLocales: AppLocale[] = ['en', 'es'];
